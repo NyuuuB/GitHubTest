8c28c72aeae494be8a051e5f2fa1fc9e27127238
NyuuuB <kmh1634311@naver.com>
2024-06-13 13:04:57 +0900

    Character

--- TestProject/Assets/Scripts/Character.cs
+++ TestProject/Assets/Scripts/Character.cs
@@ -1,15 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 
 public class Character : MonoBehaviour
 {
+    public int hpMax;
+    public int hpCurrent;
 
-
-    public bool Move(Vector3 direaction)
+    public int attackValue;
+    public int AttackValue
     {
-
-        return default;
+        get => attackValue;
+        set
+        {
+            attackValue = Mathf.Max(1 ,value);
+        }
     }
+
+    public float attackSpeed;
+    public float moveSpeed;
+    public Vector3 dir;
+
+    public Rigidbody rb;
+
+    public virtual void Attack()
+    {
+        
+    }
+    public void OnMove(InputValue value)
+    {
+        Move(value.Get<Vector3>());
+    }
+    public virtual void Move(Vector3 dir)
+    {
+        this.dir = dir.normalized;
+    }
+    private void Update()
+    {
+        rb.AddForce(moveSpeed * Time.deltaTime * dir);        
+    }
+
+
+
+    
 }
